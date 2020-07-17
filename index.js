964e9fea24dfd63b576c0c0c4fa712501418e233
Joe B. Lewis <lewis.joe.18@gmail.com>
2020-07-18 02:09:57 +0530

    enabled es6 imports and generated prisma client

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-const express = require('express')
+import express from 'express'
+import PrismaClient from '@prisma/client'
+// or const { PrismaClient } = require('@prisma/client')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const prisma = new PrismaClient.PrismaClient()
+
+const main = async () => {
+    const allUsers = await prisma.user.findMany()
+    console.log(allUsers)
+}
+
+main()
+
+app.get('/', async (req, res) => {
+    
+    res.send('Hello World!')
+})
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
