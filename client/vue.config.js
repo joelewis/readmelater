7c90ea2de190781515e529c65edb3d4745b98a6b
Joe B. Lewis <lewis.joe.18@gmail.com>
2020-07-18 11:56:34 +0530

    vue-cli client setup

---
+++ client/vue.config.js
@@ -0,0 +1,6 @@
+// vue.config.js
+module.exports = {
+    baseUrl: '/static/',
+    outputDir: '/Users/joe-2744/personal/readmelater/public',
+    indexPath: 'index.html'
+}
--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 import PrismaClient from '@prisma/client'
-// or const { PrismaClient } = require('@prisma/client')
 const app = express()
 const port = 3000
 
-const prisma = new PrismaClient.PrismaClient()
+app.use('/static', express.static('public'))
 
-const main = async () => {
-    const allUsers = await prisma.user.findMany()
-    console.log(allUsers)
-}
-
-main()
+// const prisma = new PrismaClient.PrismaClient()
+// const main = async () => {
+//     const allUsers = await prisma.user.findMany()
+//     console.log(allUsers)
+// }
+// main()
 
 app.get('/', async (req, res) => {
-    
-    res.send('Hello World!')
+    // res.send('Hello World!')
+    res.sendFile(path.join( __dirname + '/public/index.html'));
 })
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
