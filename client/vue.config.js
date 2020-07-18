44c7c4918a0fdbbb4160e3e300cc58ac73ee01b8
Joe B. Lewis <lewis.joe.18@gmail.com>
2020-07-18 23:19:53 +0530

    sample envs and auth related changes

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,8 +1,6 @@
 // vue.config.js
 var dotenv =require('dotenv');
-
 dotenv.config();
-console.log(process.env);
 module.exports = {
     baseUrl: '/static/',
     outputDir: process.env.OUTPUT_DIR, //  || '/Users/joe-2744/personal/readmelater/public',
