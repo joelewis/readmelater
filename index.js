7a18fa7466bcb40a80238ca2092cf2b2a875ad53
Joe B. Lewis <lewis.joe.18@gmail.com>
2020-07-19 13:56:42 +0530

    removed google ids from vcs

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ var JWTstrategy = PassportJwt.Strategy;
 var ExtractJwt = PassportJwt.ExtractJwt;
 
 // import from env
-const GOOGLE_CLIENT_ID = '877323248885-3lf5c6ms7crannivpombjdbqh5lqbtrp.apps.googleusercontent.com';
-const GOOGLE_CLIENT_SECRET = 'WyizCq6jvb8HMHp-RkfnZgIt';
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID; 
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 
 var GoogleStrategy = PassportGoogleOauth.OAuth2Strategy;
 
